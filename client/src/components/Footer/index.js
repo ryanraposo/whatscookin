c8cd4358d820cd6b718f73885fdc6b28d49b5be8
Ryan Raposo <raposo.ryan@gmail.com>
2022-10-07 11:30:37 -0400

    add Header, Footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container">&copy;2020</div>
+    </footer>
+  );
+};
+
+
+export default Footer;
