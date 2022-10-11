29662df8b9203c48a39b33ca0b84e42cbf969289
Ryan Raposo <raposo.ryan@gmail.com>
2022-10-11 15:17:23 -0400

    style auth forms

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -3,6 +3,8 @@ import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
+import Button from 'react-bootstrap/Button';
+
 
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
@@ -32,7 +34,7 @@ const Signup = () => {
   };
   
   return (
-    <main className='flex-row justify-center mb-4'>
+    <main className='d-flex flex-row justify-content-center mb-4 mt-4'>
       <div className='col-12 col-md-6'>
         <div className='card'>
           <h4 className='card-header'>Sign Up</h4>
@@ -65,9 +67,8 @@ const Signup = () => {
                 value={formState.password}
                 onChange={handleChange}
               />
-              <button className='btn d-block w-100' type='submit'>
-                Submit
-              </button>
+              <br/>
+              <Button className="mt-3" variant="primary" type="submit">Submit</Button>
             </form>
             {error && <div>Sign up failed</div>}
           </div>
