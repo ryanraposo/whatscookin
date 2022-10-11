9597cb2d3f086efb0e425fe693f9c525916e2b76
Ryan Raposo <raposo.ryan@gmail.com>
2022-10-11 15:05:58 -0400

    style feed

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -6,20 +6,20 @@ const PostList = ({ posts, title }) => {
   }
 
   return (
-    <div>
-      <h3>{title}</h3>
+    <div className="mt-3">
+      {title && (<h3>{title}</h3>)}
       {posts &&
         posts.slice(0).reverse().map((post) => (
           <div key={post._id} className="card mb-3">
             <div className="card-header">
-              <Link to={`/post/${post._id}`}>
+              <Link to={`/post/${post._id}`} className="card-link">
                 <h5>{post.postTitle}</h5>
               </Link>
               <p>
                 {post.createdAt} (
                 <Link
+                  className="card-link card-link-heavy"
                   to={`/profile/${post.username}`}
-                  style={{ fontWeight: 700 }}
                 >
                   {post.username}
                 </Link>
@@ -30,7 +30,7 @@ const PostList = ({ posts, title }) => {
               {<div dangerouslySetInnerHTML={{ __html: post.postBody }} />}
             </div>
             <div className="card-body">
-              <Link to={`/post/${post._id}`}>
+              <Link className="card-link" to={`/post/${post._id}`}>
                 <p>{post.postText}</p>
                 <p className="mb-0">Add Comment {post.commentCount}</p>
               </Link>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,5 @@
 import { useQuery } from '@apollo/client';
 import { QUERY_POSTS } from '../utils/queries';
-import Auth from '../utils/auth';
 
 import PostList from '../components/PostList';
 
@@ -11,16 +10,14 @@ const Home = () => {
   const posts = data?.posts || [];
   console.log(posts);
 
-  const loggedIn = Auth.loggedIn();
-
   return (
     <main>
       <div className='flex-row justify-space-between'>
-        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
+        <div className="col-12 mb-3">
           {loading ? (
             <div>Loading...</div>
           ) : (
-            <PostList posts={posts} title="Feed" />
+            <PostList posts={posts} />
           )}
         </div>
       </div>
